71e2e0078dfd21eb0ec24a21530fa3dd133f2a63
DrSav <Drsav100@gmail.com>
2023-03-16 18:59:19 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - a command that prints a line with the puts function
+ * Return: 0 (sucessful)
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle.\n");
