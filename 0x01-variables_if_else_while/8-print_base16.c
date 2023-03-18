7ac798c66fd597c9722ad5755d85c9c8215ebbb2
DrSav <Drsav100@gmail.com>
2023-03-17 19:59:04 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char x = 'a';
+	int y = 0;
+
+	while (y<10)
+	{
+		putchar(y+'0');
+		y +=1;
+	}
+	while (x <= 'f')
+	}
+		putchar(y+'0');
+		x +=1;
+	}
+	putchar (10);
+	return (0);
+}
+
