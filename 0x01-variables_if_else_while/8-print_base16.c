665c903465e35862a8ac20e48cba2eb0613fb080
DrSav <Drsav100@gmail.com>
2023-03-17 20:01:41 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main
- * Return: 0 (successful)
+ * main - entry point
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -9,17 +9,17 @@ int main(void)
 	char x = 'a';
 	int y = 0;
 
-	while (y<10)
+	while (y < 10)
 	{
-		putchar(y+'0');
-		y +=1;
+		putchar(y + '0');
+		y += 1;
 	}
 	while (x <= 'f')
+	{
+		putchar(x);
+		x += 1;
 	}
-		putchar(y+'0');
-		x +=1;
-	}
-	putchar (10);
+	putchar(10);
 	return (0);
 }
 
